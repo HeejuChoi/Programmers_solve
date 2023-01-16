a3615f751009075330321586b8ca824b28e55e6f
최희주 <110644860+HeejuChoi@users.noreply.github.com>
2023-01-16 20:29:57 +0900

    23.01.16.

---
+++ Lv.0/08.짝수는 싫어요.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = [];
+    for (let i = 1; i<=n; i++) {
+        // 홀수
+        if (i%2==1) 
+            answer.push(i)
+    }
+    return answer;
+}
+
+// arr.push(i)
+// arr에 i를 넣는다. 
+
