e044e237bd18507821b989eac0ea4d1a957cbf7a
최희주 <110644860+HeejuChoi@users.noreply.github.com>
2023-01-23 17:01:59 +0900

    23.01.23.

---
+++ Lv.0/15.배열의 평균값.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var answer = 0;
+    for (let i = 0; i<=numbers.length-1; i++) {
+        answer += numbers[i]
+    }
+    answer = answer / numbers.length
+    return answer;
+}
